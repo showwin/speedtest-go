024a7ee5d6a19c1a2fe9bff4043b9b5159442293
showwin <showwin_kmc@yahoo.co.jp>
2015-10-23 10:06:53 +0900

    download test in parallel

--- download.go
+++ download.go
@@ -6,11 +6,67 @@ import (
 	"net/http"
 	"strconv"
 	"time"
+	"sync"
 
-	"github.com/cheggaaa/pb"
+	//"github.com/cheggaaa/pb"
 )
 
+var totalSizeTmp = 0.0
+var totalSize = 0.0
+var totalTime = time.Duration(0)
+var finished = false
+
 func DownloadSpeed(dlUrl string) float64 {
+	wg := new(sync.WaitGroup)
+	m := new(sync.Mutex)
+	startTime := time.Now()
+	for i := 0; i < 4; i++ {
+		wg.Add(1)
+		go loopRequest(wg, m, startTime, dlUrl)
+	}
+	wg.Wait()
+	fmt.Println("total size")
+	fmt.Println(totalSize)
+	return totalSize * 8 / totalTime.Seconds()
+}
+
+func loopRequest(wg *sync.WaitGroup, m *sync.Mutex, startTime time.Time, dlUrl string) {
+	for {
+		sendRequest(wg, m, startTime, dlUrl)
+		if finished {
+			break
+		}
+	}
+}
+
+func sendRequest(wg *sync.WaitGroup, m *sync.Mutex, startTime time.Time, dlUrl string) {
+	size := 1000
+	url := dlUrl + "/random" + strconv.Itoa(size) + "x" + strconv.Itoa(size) + ".jpg"
+	resp, err := http.Get(url)
+	CheckError(err)
+	ioutil.ReadAll(resp.Body)
+	defer resp.Body.Close()
+
+	updateTotalSize(size, wg, m, startTime)
+}
+
+func updateTotalSize(size int, wg *sync.WaitGroup, m *sync.Mutex, startTime time.Time) {
+	m.Lock()
+	defer m.Unlock()
+	totalSizeTmp = totalSizeTmp + 2*float64(size)*float64(size)/1000/1000
+	if time.Now().After(startTime.Add(10 * time.Second)) {
+		wg.Done()
+		if finished == false {
+			finished = true
+			totalSize = totalSizeTmp
+			totalTime = time.Now().Sub(startTime)
+		}
+	}
+}
+
+/*
+
+func aaa() {
 	fmt.Println("Testing Download Speed ...")
 	count := 40 * (40 + 1) / 2
 	bar := pb.StartNew(count)
@@ -49,3 +105,5 @@ func DownloadSpeed(dlUrl string) float64 {
 
 	return totalSize * 8 / totalTime.Seconds()
 }
+
+*/
