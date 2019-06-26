1a985d9eb53a3d9cafc6aff30adc08d1c71befce
Sébastien B <{ID}+{username}@users.noreply.github.com>
2019-06-26 11:11:10 +0200

    Merge

--- upx.sh
+++ upx.sh
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-#!/bin/bash
-set -ex
-upx dist/speedtest-go*/speedtest-go*
-||||||| merged common ancestors
-=======
 #!/bin/bash
 set -ex
 upx --brute dist/speedtest-go*/speedtest-go*
->>>>>>> f358b0c6ffbb7e47312ad7165520d0baa4060fa6
