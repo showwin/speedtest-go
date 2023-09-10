6819a809f93c83e59833abd6e050bdb991703ddb
r3inbowari <r3inbowari@gmail.com>
2023-09-10 12:34:25 +0800

    fix: unexpected signal kill on some linux releases

--- speedtest/data_manager.go
+++ speedtest/data_manager.go
@@ -176,9 +176,10 @@ func (f *funcGroup) Start(cancel context.CancelFunc, mainRequestHandlerIndex int
 	dbg.Printf("auxN: %d\n", auxN)
 	wg := sync.WaitGroup{}
 	f.manager.running = true
-	ticker := f.manager.rateCapture()
+	stopCapture := f.manager.rateCapture()
 	time.AfterFunc(f.manager.captureTime, func() {
-		ticker.Stop()
+		stopCapture <- true
+		close(stopCapture)
 		f.manager.running = false
 		cancel()
 		dbg.Println("FuncGroup: Stop")
@@ -220,15 +221,16 @@ func (f *funcGroup) Start(cancel context.CancelFunc, mainRequestHandlerIndex int
 	wg.Wait()
 }
 
-func (dm *DataManager) rateCapture() *time.Ticker {
+func (dm *DataManager) rateCapture() chan bool {
 	ticker := time.NewTicker(dm.rateCaptureFrequency)
 	oldTotalDownload := dm.totalDownload
 	oldTotalUpload := dm.totalUpload
-	go func() {
-	loop:
+	stopCapture := make(chan bool)
+	go func(t *time.Ticker) {
+		defer t.Stop()
 		for {
 			select {
-			case <-ticker.C:
+			case <-t.C:
 				newTotalDownload := dm.totalDownload
 				newTotalUpload := dm.totalUpload
 				deltaDownload := newTotalDownload - oldTotalDownload
@@ -241,14 +243,14 @@ func (dm *DataManager) rateCapture() *time.Ticker {
 				if deltaUpload != 0 {
 					dm.UploadRateSequence = append(dm.UploadRateSequence, deltaUpload)
 				}
-			default:
-				if !dm.running {
-					break loop
+			case stop := <-stopCapture:
+				if stop {
+					return
 				}
 			}
 		}
-	}()
-	return ticker
+	}(ticker)
+	return stopCapture
 }
 
 func (dm *DataManager) NewChunk() Chunk {
